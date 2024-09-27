4b376937922b8e62ade52734d0f47ca5d1b37dcf
alphay2 <mingfuk.inv@gmail.com>
2024-09-27 14:35:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,3 +28,15 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+let responseObject = {}
+app.enable('trust proxy')
+app.get('/api/whoami', (request, response) => {
+  responseObject['ipaddress'] = request.ip
+  responseObject['language'] = request.get('Accept-Language')
+  responseObject['software'] = request.get('User-Agent')
+
+  response.json(responseObject)
+})
